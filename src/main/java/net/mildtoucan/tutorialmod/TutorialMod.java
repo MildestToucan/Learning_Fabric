84db64e0575545f3511ed10d6accb760b9718508
MildToucan <introvertpale@gmail.com>
2025-04-14 01:28:57 +0200

    Testing commits

--- src/main/java/net/mildtoucan/tutorialmod/TutorialMod.java
+++ src/main/java/net/mildtoucan/tutorialmod/TutorialMod.java
@@ -2,15 +2,17 @@ package net.mildtoucan.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mildtoucan.tutorialmod.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//Testing Github commits
+
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 
 	}
 }
