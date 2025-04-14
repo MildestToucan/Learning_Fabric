16ecef2a72b0185c1d89e3ed571ed41a2d38363f
MildToucan <introvertpale@gmail.com>
2025-04-14 18:16:22 +0200

    Slight changes to syntax

--- src/main/java/net/mildtoucan/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mildtoucan/tutorialmod/block/ModBlocks.java
@@ -18,7 +18,10 @@ public class ModBlocks {
                     .strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
-
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool().sounds(BlockSoundGroup.TUFF)));
 
 
     private static Block registerBlock(String name, Block block) {
@@ -37,6 +40,7 @@ public class ModBlocks {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.PINK_GARNET_BLOCK);
+            entries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
         });
     }
 }
