1804ca8b95f9637e974e7fbca51178b188ab1acb
MildToucan <introvertpale@gmail.com>
2025-04-18 02:14:51 +0200

    Added Starlight Ashes, a fuel source that burns for 600 ticks (3 standard item recipes) Added cauliflower, a food item that can occasionally give 10 seconds (200 ticks) of haste

--- src/main/java/net/mildtoucan/tutorialmod/TutorialMod.java
+++ src/main/java/net/mildtoucan/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.mildtoucan.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.mildtoucan.tutorialmod.block.ModBlocks;
 import net.mildtoucan.tutorialmod.item.ModItemGroups;
 import net.mildtoucan.tutorialmod.item.ModItems;
@@ -18,5 +19,11 @@ public class TutorialMod implements ModInitializer {
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroups();
 
+		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
+		/* Look for AbstractFurnaceBlockEntity in the Minecraft libraries to find the fuel times of the Vanilla items.
+		 * The standard item smelts/cooks in 200 Ticks!
+		 * So your fuel time divided by 200 is the number of items it can smelt in a standard furnace recipe.
+		 * In the case of having to add several Fuel items, you can make a special ModFuelItems Class that you can then
+		 * call upon here. This will make the onInitialize Method much cleaner.*/
 	}
 }
