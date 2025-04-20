58116c39e4b6f1d18b651d0f625e9e5b4d9a9b02
MildestToucan <introvertpale@gmail.com>
2025-04-20 20:05:10 +0200

    Added tooltips to ChiselItem, MagicBlock, and Cauliflower.

--- src/main/java/net/mildtoucan/tutorialmod/item/ModItems.java
+++ src/main/java/net/mildtoucan/tutorialmod/item/ModItems.java
@@ -5,9 +5,14 @@ import net.mildtoucan.tutorialmod.TutorialMod;
 import net.mildtoucan.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItems {
 
@@ -17,7 +22,13 @@ public class ModItems {
     public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
 
     public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings()
-            .food(ModFoodComponents.CAULIFLOWER)));
+            .food(ModFoodComponents.CAULIFLOWER)) {
+        @Override
+        public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.cauliflower"));
+            super.appendTooltip(stack, context, tooltip, type);
+        }
+    }); //Item tooltip through an Anonymous Class, if the item doesn't have its own custom Class.
 
     public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes", new Item(new Item.Settings()));
 
--- src/main/java/net/mildtoucan/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/net/mildtoucan/tutorialmod/item/custom/ChiselItem.java
@@ -3,16 +3,21 @@ package net.mildtoucan.tutorialmod.item.custom;
 import net.mildtoucan.tutorialmod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Map;
 
 public class ChiselItem extends Item {
@@ -49,5 +54,16 @@ public class ChiselItem extends Item {
 
 
         return ActionResult.SUCCESS; //Tells the game the action went through as expected. Makes the player do a right click animation
+    }
+
+    @Override //This here will contain a tooltip that requires that you press shift to reveal it.
+    public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.chisel_shift_down"));
+        } else {
+            tooltip.add(Text.translatable("tooltip.tutorialmod.chisel"));
+        }
+
+        super.appendTooltip(stack, context, tooltip, type);
     }
 }
